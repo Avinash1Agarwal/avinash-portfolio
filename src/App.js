67fefefdc1e543c0ebf406f92cbda99aaa68e57f
Avinash1Agarwal <avinash12261999@gmail.com>
2025-08-06 00:27:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react
 import { motion, AnimatePresence } from "framer-motion";
 import { Skills, Projects } from "./SkillsProjects";
 
-<Route path="/skills" element={<Skills/>}/>
-<Route path="/projects" element={<Projects/>}/>
+<Route path="/skills" element={<Skills />} />
+<Route path="/projects" element={<Projects />} />
 
 const pageVariants = {
   initial: { opacity: 0, y: 30 },
